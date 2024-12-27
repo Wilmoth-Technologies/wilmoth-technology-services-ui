6eef61bc196fd75d222c140f405d5599c9f3e091
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2024-12-27 09:09:05 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Wilmoth-Technologies/wilmoth-technology-services-ui",
+  base: "/wilmoth-technology-services-ui/",
   plugins: [react()],
   preview: {
     port: 3000,
